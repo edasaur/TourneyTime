2829897db74bf82f5d7c7ad95ddd3515d6432249
edasaur <edx.wang@gmail.com>
2016-04-25 14:01:40 -0700

    Auth done. Need to be able to register users tho lol

--- TourneyTime/urls.py
+++ TourneyTime/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# authtest/urls.py
+from django.conf.urls import include, url
 from django.contrib import admin
+# Add this import
+from django.contrib.auth import views
+from log.forms import LoginForm
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('log.urls')),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}),  
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),  
 ]
